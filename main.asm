b548f84c4071a51bffd06c169d3c321007f09a17
xeno <xeno.freenode@gmail.com>
2019-12-16 21:03:31 +0100

    work in (very slight) progress

--- main.asm
+++ main.asm
@@ -1,29 +1,59 @@
 %include "syscalls.mac"
-
+default rel
 global start
 section .text
 start:
    
-   ; open(filename, RO, -)
-    mov rax,  SYS_OPEN
-    mov rdi,  msg ; filename
-    mov rsi,  O_RDONLY ;  flags=ro
+    mov rax, SYS_OPEN
+    mov rdi, filename 
+    mov rsi, O_RDONLY ;  flags=ro
     mov rdx, 0 ; mode, not used
     syscall
+    mov  qword [input_fd], rax
 
-   ; write(1, msg, len)
+    mov rax, SYS_OPEN
+    mov rdi, outfile
+    mov rsi, O_CREATE_WRITE ;  flags=ro
+    mov rdx, 0666o; mode
+    syscall
+    mov [output_fd], rax
+
+    ; write to stdout
     mov rax, SYS_WRITE
     mov rdi, FD_STDOUT
     mov rsi, msg
     mov rdx, msg.len
     syscall
 
+    output_digit FD_STDOUT
+    output_newline
+    output_digit [output_fd]
+    output_newline
+
+    ; write to output file
+    mov rax, SYS_WRITE
+    mov rdi, [output_fd]
+    mov rsi, msg
+    mov rdx, msg.len
+    syscall
+
+    ; close it 
+    mov rax, SYS_CLOSE
+    mov rdi, [output_fd]
+    syscall
+
     ; exit(0)
     mov rax, SYS_EXIT ; exit
-    mov rdi,  EXIT_SUCCESS
+    mov rdi, EXIT_SUCCESS
     syscall
 
 section .data
+
 msg:        db    "Hello, world!", 10
-.len:         equ     $ - msg
-filename: db    "input.txt"
+.len:       equ   $ - msg
+filename:   db    "input.txt"
+outfile:    db    "output.txt"
+input_fd:   dq     0x0
+output_fd:  dq     0x2
+digits:     db     "0123456789_123456789_123456789"
+newline:    db     10
