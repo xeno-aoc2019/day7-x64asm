7f1e72f5a36602516e0689d023e95e175670f7c6
xeno <xeno.freenode@gmail.com>
2019-12-17 18:13:49 +0100

    ...

--- main.asm
+++ main.asm
@@ -26,11 +26,6 @@ start:
     mov rdx, msg.len
     syscall
 
-    output_digit FD_STDOUT
-    output_newline
-    output_digit [output_fd]
-    output_newline
-
     ; write to output file
     mov rax, SYS_WRITE
     mov rdi, [output_fd]
@@ -43,37 +38,51 @@ start:
 
     ;; experiment
 
-    mov r11, 0xa4bc ; should be output as 0xa4bc
+    output_sepline
+
+    mov r11, 0xc0
+    shr r11, 4
+    output_digit r11
+
+    output_newline
+    output_sepline
+
+    mov r12, 0xa4bc ;
+    mov r11, r12
 
     mov rax, SYS_WRITE
     mov rdi, FD_STDOUT
     mov rdx, 1 ; length 
     mov r10, r11 ; r10 = r11
     and r10, 0xf000
-    shr r10, 24
+    shr r10, 12
     mov rsi, hexdigits
     add rsi, r10 ; adding the hex digit...
     syscall
+    mov r11, r12 ; should be output as 0xa4bc
 
     mov rax, SYS_WRITE
     mov rdi, FD_STDOUT
     mov rdx, 1 ; length 
     mov r10, r11 ; r10 = r11
     and r10, 0x0f00
-    shr r10, 16
+    shr r10, 8
     mov rsi, hexdigits
     add rsi, r10 ; adding the hex digit...
     syscall
+    mov r11, r12 ; should be output as 0xa4bc
 
     mov rax, SYS_WRITE
     mov rdi, FD_STDOUT
     mov rdx, 1 ; length 
     mov r10, r11 ; r10 = r11
     and r10, 0x00f0
-    shr r10, 8
+    shr r10, 4
     mov rsi, hexdigits
     add rsi, r10 ; adding the hex digit...
     syscall
+
+    mov r11, r12 ; should be output as 0xa4bc
 
     mov rax, SYS_WRITE
     mov rdi, FD_STDOUT
@@ -85,6 +94,12 @@ start:
     syscall
     output_newline
 
+    output_hex64 0x1234
+    output_newline
+    output_hex64 0xa1b2
+    output_newline
+    output_hex64 0xf951
+    output_newline
 
     ; output_digit r11
     ; output_newline
@@ -100,8 +115,9 @@ msg:        db    "Hello, world!", 10
 .len:       equ   $ - msg
 filename:   db    "input.txt",0
 outfile:    db    "output.txt",0
-input_fd:   dq     0x0
-output_fd:  dq     0x0
-digits:     db     "123456789_123456789_123456789"
-newline:    db     10
-hexdigits:  db     "0123456789abcdef***" 
+input_fd:   dq    0x0
+output_fd:  dq    0x0
+digits:     db    "123456789_123456789_123456789"
+newline:    db    10
+hexdigits:  db    "0123456789abcdef***" 
+debug_line: db    "----------",10
