89a193e9dd04d21f927836d0bc32b46483c660cd
xeno <xeno.freenode@gmail.com>
2019-12-21 12:50:18 +0100

    work in progress

--- parsefile.asm
+++ parsefile.asm
@@ -5,12 +5,19 @@
 %include "printd.inc"
 %include "memalloc.inc"
 
+%define COMMA   44
+%define NEWLINE 10
+%define ZERO    0x30
+
 %macro debug_num 1
     mov r10, %1
     prints debug_s, debug_s.len
     printd r10
     println
 %endmacro
+
+; conv:
+; RDI, RSI, RDX, RCX, R8, R9 -> RAX
 
 default rel
 global _parsefile
@@ -22,9 +29,9 @@ disp_bytes: ; rax
     push r11
     push r12
     xor r10, r10
+    ; r10 = byte_count
 .loop:
-    cmp r10, 8
-    push r10
+    cmp r10, 8   ; if byte_code = 8 goto end
     je .end
     
     mov r12, rax ; store the value
@@ -33,25 +40,92 @@ disp_bytes: ; rax
     
     cmp rdx, 0
     je .null
-    push rdx
-    prints dot, 1
-    pop rdx
+    cmp rdx, 10 ; newline
+    je .null
+    cmp rdx, COMMA
+    je .comma
     printd rdx
+    jmp .printed
+.comma:
+    prints comma, 1
+    jmp .printed
+.printed:
     prints space, 1
     mov rax, r12 ; restore the value
     shr rax, 8 ; <- shift to next byte
-    pop r10
     inc r10
     jmp .loop
 .null:
-    prints null, null.len
+    prints null, 4
 .end:
     println
+    pop r10
+    pop r11
+    pop r12
+    ret
+
+process_bytes: ; rax = curr_val, rsi = int
+    push r10
+    push r11
+    push r12    
+    xor r10, r10 
+    mov r12, 10
+    ; r12 = CONST=10
+    ; r10 = byte_count
+    ; rax = curr_val
+    ; rsi = int
+    ; rdx = current_char/digit
+.loop:
+    cmp r10, 8 ; if byte_count = 8 goto end (byte finished)
+    je .end
+
+    mov rdx, rsi   ; current_char = int.last_byte
+    and rdx, 255 
+
+    cmp rdx, 0     ; if current_char = NULL goto null
+    je .null 
+    cmp rdx, 10    ; if current_char = NEWLINE goto null
+
+    cmp rdx, COMMA ; if current_char = COMMA goto comma
+    je .comma
+
+    sub rdx, ZERO ; current_char.int_from_ascii
+
+    mul r12        ; curr_val = curr_val*10+current_char/digit
+    add rax, rdx 
+
+    inc r10        ; byte_count++
+    jmp .loop       ; continue to next char
+
+.comma:
+    mov r11, [output_buf]    ; output_buf[output_iter] = current_val
+    mov r12, [output_iter]
+    add r11, r12
+    mov [r11], rax
+
+    inc r12                  ; output_iter++
+    mov [output_iter], r12
+
+    xor rax, rax             ; current_val = 0 (next value)
+    inc r10                  ; byte_count++
+    jmp .loop                 ; continue to next char
+
+.null:
+    mov r11, [output_buf]    ; output_buf[output_iter] = current_val
+    mov r12, [output_iter]
+    add r11, r12
+    mov [r11], rax
+
+    inc r12                  ; output_iter++
+    mov [output_iter], r12
+
+    prints null, 4
+    println
+.end:
     pop r12
     pop r11
     pop r10
     ret
-
 
 ; parsefile rax=input_fd
 _parsefile:
@@ -71,19 +145,12 @@ _parsefile:
     mov r12, [r12]
     mov rax, r12
     call disp_bytes 
+    println
     debug_num 10002
-
-    debug_num r10
-    mov r11, r10
-    mov r12, [_0]
-    sub r11, r12
-    debug_num r11
-    mov r10, input_buf
-    inc r10
-    mov r11, [r10]
-    sub r11, r12
-    debug_num r11 
-
+    xor rax, rax ; current_val = 0
+    mov rsi, r12 ; int 
+    call process_bytes
+    debug_num 10003
 .end_of_file:
 
 
@@ -94,6 +161,7 @@ input_buf       dq    0x0 ; input parsing buffer
 input_iter      dq    0x0 ; pointer while iterating
 input_size      dq    0x0 ; size of the last read
 output_buf      dq    0x0 ; parsed program
+output_iter     dq    0x0 ; pointer while iterating
 debug_s:        db  "debug: "
 .len:           equ   $ - debug_s
 newline:        db    0x0a
@@ -104,3 +172,5 @@ space:          db    " "
 null:           db    "null"
 .len            db    $ - null
 dot:            db    "."
+par_end         db    "]"
+par_start       db    "["
