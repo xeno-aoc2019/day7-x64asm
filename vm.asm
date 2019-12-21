3774468a9db0bbfae21e6fe1bd5717b176ed5a89
xeno <xeno.freenode@gmail.com>
2019-12-21 22:57:16 +0100

    ...

--- vm.asm
+++ vm.asm
@@ -25,13 +25,17 @@ get_opcode: ; rax -> rax
     pop r14
     ret
 
-copy_program: ; [orig_program_p, rcx = size
-    printd 100000
-    prints colon, 1
-    mov r13, rcx
-    printd r13
+copy_program: ; rax = vm id (0-4), rcx = program size
+    push r15
+    mov r15, 8
+    mul r15 ; rax = rax * 8 (vm id to byte offset)
+    mov r15, rax ; r15 = byte offset
+    printd r15
     println
-    memalloc [program_p], r13
+    mov r13, rcx ; r13 = program_size
+    lea r10, [rel program_p]
+ ;   add r10, r15 ; r10 = program_p[r15]
+    memalloc [program_p], r13 
     xor r11, r11
     mov r12, 0
 .loop:
@@ -48,6 +52,7 @@ copy_program: ; [orig_program_p, rcx = size
     add r12, 1
     jmp .loop
 .done:
+    pop r15
     ret
 
 
@@ -56,6 +61,7 @@ _vm_init: ; rax = *vm, *rcx = *size
     mov [program_size], rcx
     printd rax
     println
+    xor rax, rax ; rax = 0 (vm id)
     call copy_program
     xor rax, rax
     add rax, 1
@@ -63,9 +69,9 @@ _vm_init: ; rax = *vm, *rcx = *size
     ret
 
 section .data
-orig_program_p:     dq  0x0
-program_p           dq  0x0
-program_size:       dq  0x0
+orig_program_p:     dq  0x0 ; there's only one original program
+program_p           dq  0x0,0x0,0x0,0x0,0x0 ; but 5 copies
+program_size:       dq  0x0 ; all of the same size
 
 newline             db  10
 space               db  0x20
